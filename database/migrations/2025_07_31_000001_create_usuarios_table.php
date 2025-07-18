1fc2aa18e0178179d66a395ab35604a2b3494939
AppreDeve <136930179+AppreDeve@users.noreply.github.com>
2025-07-18 09:41:29 -0700

    ajuste de la migracion usuario table

--- database/migrations/2025_07_31_000001_create_usuarios_table.php
+++ database/migrations/2025_07_31_000001_create_usuarios_table.php
@@ -8,12 +8,10 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_admin')->default(false);
             $table->rememberToken();
